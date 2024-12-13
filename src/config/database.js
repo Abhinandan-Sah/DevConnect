c9e521424265268d8c49ff16dd48e8254dcd5e13
Abhinandan Sah <abhinandansah222@gmail.com>
2024-12-14 01:50:43 +0530

    update the url

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://abhinandansah222:xelGq7nDv3v1zgb5@devconnect-cluster.qfhxv.mongodb.net/devConnect")
+    await mongoose.connect(DB_URL)
 }
 
 module.exports = connectDB;
