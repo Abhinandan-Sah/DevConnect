27dbd308546b0fa599f7dae369821081c7e5ce07
Abhinandan Sah <abhinandansah222@gmail.com>
2025-02-19 23:37:11 +0530

    up

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -8,7 +8,7 @@ profileRouter.get("/profile", userAuth , async(req, res)=>{
     try{
         const user = req.user;
         res.send(user)
-
+        
     }catch(err){
         res.status(400).send("Error : "+err.message);
     }
@@ -52,6 +52,6 @@ profileRouter.patch("/profile/password", userAuth, async(req, res)=>{
     }catch(err){
         res.status(400).send("ERROR: "+ err.message)
     }
-})
+});
 
 module.exports = profileRouter;
