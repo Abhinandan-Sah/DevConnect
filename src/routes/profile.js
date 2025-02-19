6db397655fe2b2fe461d0d1c1ac2aab376decbbc
Abhinandan Sah <abhinandansah222@gmail.com>
2025-02-19 23:36:15 +0530

    update

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -32,10 +32,25 @@ profileRouter.patch("/profile/edit", userAuth, async(req, res)=>{
 });
 
 profileRouter.patch("/profile/password", userAuth, async(req, res)=>{
-    try{
+    // try{
         
+    // }catch(err){
+    //     res.status(400).send("ERROR: "+err.message);
+    // }
+
+    try{
+        if(!validateEditProfileData(req)){
+            throw new Error("Invalid Edit Request");
+        }
+
+        const loggedInUser = req.user;
+
+        Object.keys(req.body).forEach((key) =>loggedInUser[key]=req.body[key]);
+        await loggedInUser.save();
+
+        res.json({message: `${loggedInUser.firstName}, your pofile edited successfully`, data: loggedInUser});
     }catch(err){
-        res.status(400).send("ERROR: "+err.message);
+        res.status(400).send("ERROR: "+ err.message)
     }
 })
 
