55511e5d05becaee30d5062c177736bf88ed2933
Abhinandan Sah <abhinandansah222@gmail.com>
2025-02-23 23:39:16 +0530

    add error handling and status validation for connection request review

--- src/routes/request.js
+++ src/routes/request.js
@@ -33,7 +33,19 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async(requestRou
 }});
 
 requestRouter.post("/request/review/:status/:requestId", userAuth, async(req, res)=>{
+    try{
+        const loggedInUser = req.user;
+        const {status, requestId} = req.params;
 
+        const allowedStatus = ["accepted", "rejected"];
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({message: "Invalid status type: "+ status});
+        }
+
+        const connectionRequest = await ConnectionRequestModel.findOne({_id: requestId, toUserId: loggedInUser._id});
+    }catch(err){
+        res.status(400).send("ERROR: " + err.message);
+    }
 });
 
 module.exports = requestRouter;
