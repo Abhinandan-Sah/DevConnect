f730d974391dc67e7595921ff4ff4a2522824f57
Abhinandan Sah <abhinandansah222@gmail.com>
2025-03-05 10:41:16 +0530

    implement password change functionality with strong password validation

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const userModel = require("../models/user");
 const profileRouter= express.Router();
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 profileRouter.get("/profile", userAuth , async(req, res)=>{
 
@@ -32,26 +35,40 @@ profileRouter.patch("/profile/edit", userAuth, async(req, res)=>{
 });
 
 profileRouter.patch("/profile/password", userAuth, async(req, res)=>{
-    // try{
-        
-    // }catch(err){
-    //     res.status(400).send("ERROR: "+err.message);
-    // }
-
     try{
-        if(!validateEditProfileData(req)){
-            throw new Error("Invalid Edit Request");
+        const newPassword = req.body.password;
+        const user = req.user;
+        if(!validator.isStrongPassword(newPassword)){
+            throw new Error("Enter a strong Password");
+        }
+        const isSamePassword = await bcrypt(newPassword, user.password);
+        if(isSamePassword){
+            throw new Error("New password cannot be same as old password");
         }
 
-        const loggedInUser = req.user;
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        await user.save();
+        res.json({message: "Password changed successfully"})
 
-        Object.keys(req.body).forEach((key) =>loggedInUser[key]=req.body[key]);
-        await loggedInUser.save();
-
-        res.json({message: `${loggedInUser.firstName}, your pofile edited successfully`, data: loggedInUser});
     }catch(err){
-        res.status(400).send("ERROR: "+ err.message)
+        res.status(400).send("ERROR: "+err.message);
     }
+
+    // try{
+    //     if(!validateEditProfileData(req)){
+    //         throw new Error("Invalid Edit Request");
+    //     }
+
+    //     const loggedInUser = req.user;
+
+    //     Object.keys(req.body).forEach((key) =>loggedInUser[key]=req.body[key]);
+    //     await loggedInUser.save();
+
+    //     res.json({message: `${loggedInUser.firstName}, your pofile edited successfully`, data: loggedInUser});
+    // }catch(err){
+    //     res.status(400).send("ERROR: "+ err.message)
+    // }
 });
 
 module.exports = profileRouter;
--- src/routes/user.js
+++ src/routes/user.js
@@ -97,7 +97,6 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         console.log(users);
 
         res.send(hideUsersFromFeed);
-        res.send(hideUsersFromFeed);
     }
     catch(err){
         res.status(400).send("ERROR: "+ err.message)
