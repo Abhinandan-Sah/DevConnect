0770fd1b500e9afe6e99d84efe1447a4e80233a5
Abhinandan Sah <abhinandansah222@gmail.com>
2025-03-12 23:43:16 +0530

    feat: fetch user data on Body component mount and dispatch to store

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
+import { BASE_URL } from '../utils/constants'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const fetchUser = async () => {
+    try{
+      const response = await axios.get(BASE_URL + '/profile/view', {
+        withCredentials: true,
+      });
+      dispatch(addUser(response.data));
+
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div className='min-h-screen flex flex-col'>
       <Navbar />
