14ff2ec2d2e73ccc0d9d80f8d85467bdacb387e3
Abhinandan Sah <abhinandansah222@gmail.com>
2025-05-20 17:01:17 +0545

    up

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -11,6 +11,7 @@
 export const BASE_URL =
   location.hostname === "devconnects.tech" ||
   location.hostname === "www.devconnects.tech" ||
+  location.hostname === "localhost" ||
   location.hostname === "20.40.54.228"
     ? `http://${location.hostname}:5000`
     : "/api";
