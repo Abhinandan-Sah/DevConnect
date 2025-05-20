4b9e347e0da2aa87156e26bd976f893ae5aac689
Abhinandan Sah <abhinandansah222@gmail.com>
2025-05-20 14:39:48 +0545

    up

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -5,4 +5,5 @@
 // export const BASE_URL = "/api"
 
 // Takes dynamic hostname
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:5000": "/api";
+// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:5000": "/api";
+export const BASE_URL = location.hostname === "20.40.54.228" ? "http://20.40.54.228:5000": "/api";
