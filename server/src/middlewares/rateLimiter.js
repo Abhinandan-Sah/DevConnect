6aea156199e1fee811cc384d15077d9f36b5bad7
Abhinandan Sah <abhinandansah222@gmail.com>
2025-06-04 23:42:20 +0545

    Implement rate limiter middleware with Redis integration

--- server/src/middlewares/rateLimiter.js
+++ server/src/middlewares/rateLimiter.js
@@ -1,8 +1,19 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user");
+const redisClient = require("../config/redis");
 
-const rateLimiter = () => {
-  return (
-    <div>rateLimiter</div>
-  )
+const rateLimiter = async(req, res, next) => {
+
+  try{
+      const ip = req.ip;
+      const currentTime = Date.now();
+
+      redisClient.incr(ip)
+  }
+  catch(err) {
+    console.error("Error in rate limiter:", err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 }
 
 module.exports =rateLimiter;
