6b16672c383bc6007ef508df0df665f7f10d9e8e
Abhinandan Sah <abhinandansah222@gmail.com>
2025-07-18 22:20:53 +0530

    Refactor auth routes to simplify cookie handling and remove commented code

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -12,16 +12,13 @@ const { userAuth } = require("../middlewares/auth.js");
 
 authRouter.post("/signup", async (req, res) => {
   try {
-    // validate the data
     validateSignUpData(req);
 
     const { firstName, lastName, emailId, password, skills, photoURL, about } =
       req.body;
 
-    // Encrypt the password
     const passwordHash = await bcrypt.hash(password, 10);
 
-    // creating a new instance of user model
     const user = new userModel({
       firstName,
       lastName,
@@ -32,23 +29,13 @@ authRouter.post("/signup", async (req, res) => {
       about,
     });
     const savedUser = await user.save();
-    // Clear existing cookie (important)
-    res.clearCookie("token", {
-      httpOnly: true,
-      sameSite: process.env.SERVER_ENV === "production" ? "None" : "Lax",
-      sameSite: "None",
-    });
-    // Generate JWT Token
+
+    res.clearCookie("token");
+
     const token = await user.getJWT();
 
-    // Set the token in cookie
-    // res.cookie("token", token, {expires: new Date(Date.now()+ 8 * 3600000)});
-    // Set the token in cookie
     res.cookie("token", token, {
-      httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
-      sameSite: process.env.SERVER_ENV === "production" ? "None" : "Lax", // Allows sending cookies on top-level navigation GET requests
-      secure: process.env.SERVER_ENV === "production", // only HTTPS in prod
-      expires: new Date(Date.now() + 3600000), // 1 hours from now
+      expires: new Date(Date.now() + 3600000), // 1 hour from now
     });
     res.json({ message: "User Added Successfully!", data: savedUser });
   } catch (err) {
@@ -64,32 +51,19 @@ authRouter.post("/login", async (req, res) => {
 
     const user = await userModel.findOne({ emailId: emailId });
     if (!user) {
-      throw new Error("Invalid credentials"); // Best practice for security say Invalid credentials
+      throw new Error("Invalid credentials");
     }
 
-    // Decrypt the password
-    // const isPasswordValid = await bcrypt.compare(password, user.password);
     const isPasswordValid = await user.validatePassword(password);
     if (isPasswordValid) {
-      res.clearCookie("token", {
-        httpOnly: true,
-        sameSite: process.env.SERVER_ENV === "production" ? "None" : "Lax",
-        secure: process.env.SERVER_ENV === "production",
-      });
+      res.clearCookie("token");
 
-      // Generate JWT Token
       const token = await user.getJWT();
 
-      // Set the token in cookie
-      // res.cookie("token", token, {expires: new Date(Date.now()+ 8 * 3600000)});
       res.cookie("token", token, {
-        httpOnly: true,
-        sameSite: process.env.SERVER_ENV === "production" ? "None" : "Lax",
-        secure: process.env.SERVER_ENV === "production", // only HTTPS in prod
-        expires: new Date(Date.now() + 3600000), // optional
+        expires: new Date(Date.now() + 3600000),
       });
 
-      // res.send("Login Successful");
       console.log("Login Successful");
       res.send(user);
     } else {
@@ -100,35 +74,23 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
-// authRouter.post("/logout", async(req, res)=>{
-//     console.log("Logout")
-//     res.cookie("token", null, {
-//         expires: new Date(Date.now())
-//     });
-//     res.send("Logout Successfull!!");
-// });
 authRouter.post("/logout", userAuth, async (req, res) => {
-  
-  try{
+  try {
     const { token } = req.cookies;
-   if (!token) {
+    if (!token) {
       return res.status(401).json({ message: "No token found" });
     }
 
-  const payload = jwt.decode(token);
+    const payload = jwt.decode(token);
 
-  await redisClient.set(`token:${token}`, "Blocked");
-  await redisClient.expire(`token:${token}`, payload.exp); // 1800 is second which is 30 minutes
-  res.cookie("token", null, {
-    httpOnly: true,
-    sameSite: process.env.SERVER_ENV === "production" ? "None" : "Lax",
-    secure: process.env.SERVER_ENV === "production", // only HTTPS in prod
-    expires: new Date(Date.now()),
-  });
-  res.send("Logout Successful!!");
+    await redisClient.set(`token:${token}`, "Blocked");
+    await redisClient.expire(`token:${token}`, payload.exp);
 
-  }
-  catch (err) {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+    });
+    res.send("Logout Successful!!");
+  } catch (err) {
     console.error("Error during logout:", err);
     res.status(500).send("Internal Server Error");
   }
