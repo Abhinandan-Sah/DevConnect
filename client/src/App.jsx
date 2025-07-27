cd4458d30f65b9b70f9a7da7d50465e9248ee6c9
Abhinandan Sah <abhinandansah222@gmail.com>
2025-07-27 12:06:52 +0530

    Refactor index.html and App.jsx to implement Google Analytics tracking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,46 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Body from './components/Body'
-import Login from './components/Login'
-import Profile from './components/Profile'
-import {Provider} from "react-redux"
-import appStore from './utils/appStore'
-import Feed from './components/Feed'
-import Connections from './components/Connections'
-import Requests from './components/Requests'
-import Chat from './components/Chat'
+import { useEffect } from "react"
+import { useLocation, BrowserRouter, Route, Routes } from "react-router-dom"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore"
+import Body from "./components/Body"
+import Feed from "./components/Feed"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import Connections from "./components/Connections"
+import Requests from "./components/Requests"
+import Chat from "./components/Chat"
+
+function GoogleAnalytics() {
+  const location = useLocation()
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("config", "G-SVFF278VQ9", {
+        page_path: location.pathname,
+      })
+    }
+  }, [location])
+  return null
+}
 
 function App() {
-
   return (
     <>
-    <Provider store={appStore}>
-      <BrowserRouter basename='/'>
-        <Routes>
-          <Route path="/" element={<Body />} >
-            <Route path='/' element={<Feed />} />
-            <Route path='/login' element={<Login />} />
-            {/* <Route path='/feed' element={<Feed />} /> */}
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/connections' element={<Connections />} />
-            <Route path='/requests' element={<Requests />} />
-            <Route path='/chat/:targetUserId' element={<Chat />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>  
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <GoogleAnalytics />
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              {/* <Route path='/feed' element={<Feed />} /> */}
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+              <Route path="/chat/:targetUserId" element={<Chat />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
